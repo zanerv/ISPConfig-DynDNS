1b4cfb61a1299f0a75df5d560923d6667fe99ce6
Z <zanerv@gmail.com>
2021-07-09 19:24:58 +0200

    Create ddns.sh

---
+++ ddns.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ddns_user="dyndns"
+ddns_pass="password"
+dns=8.8.8.8
+domain=$(dnsdomainname)
+hostname=$(hostname)
+check() {
+  ping ${dns} -c 1 -i .2 >/dev/null 2>&1
+  ONLINE=$?
+}
+check
+
+i=0
+while [ ${i} -lt 10 ]; do
+  if [[ ${ONLINE} -eq 1 ]]; then
+     sleep 1m
+     check
+     continue
+  fi
+  IP=$(host $(hostname -f) ${dns} | awk '{print $4}' | tr -d '[:space:]')
+  NewIP=$(curl -s https://ifconfig.me)
+  if [[ $? -gt 0 ]]; then
+    break
+  fi
+  if [[ ${IP} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && -n ${NewIP} ]]; then
+    break
+  fi
+  sleep 5
+  i=$(( ${i} + 1 ))
+done
+if [[ ${IP} != ${NewIP} && -n ${NewIP} && -n ${IP} ]]; then
+   curl -su ${ddns_user}:${ddns_pass} "https://${domain}/dyndns.php?hostname=${hostname}&myip=${NewIP}"
+fi
